eb875c8681761332538ab1bd4ef5832c8713bf91
Sudheer Darla <sudheerkumarddsn@gmail.com>
2023-11-29 00:23:11 +0530

    Dynamically Pass Props

--- 01-starting-project/01-starting-project/src/App.jsx
+++ 01-starting-project/01-starting-project/src/App.jsx
@@ -1,9 +1,29 @@
+import { CORE_CONCEPTS } from './data';
 import reactConceptsImage from './assets/react-core-concepts.png';
-
 const reactDescriptions = ['Fundamental', 'Crucial', 'Core'];
 
 function genRandomInt(max) {
   return Math.floor(Math.random() * (max + 1));
+}
+
+// function CoreConcept(props) {
+//   return (
+//     <li>
+//       <img src={props.image} alt='' />
+//       <h2>{props.title}</h2>
+//       <p>{props.description}</p>
+//     </li>
+//   );
+// }
+
+function CoreConcept({ image, title, description }) {
+  return (
+    <li>
+      <img src={image} alt='' />
+      <h2>{title}</h2>
+      <p>{description}</p>
+    </li>
+  );
 }
 
 function Header() {
@@ -26,7 +46,19 @@ function App() {
     <div>
       <Header></Header>
       <main>
-        <h2>Time to get started!</h2>
+        <h2>Core Concepts</h2>
+        <section id='core-concepts'>
+          <ul>
+            <CoreConcept {...CORE_CONCEPTS[0]}></CoreConcept>
+            <CoreConcept {...CORE_CONCEPTS[1]}></CoreConcept>
+            <CoreConcept {...CORE_CONCEPTS[2]}></CoreConcept>
+            <CoreConcept
+              image={CORE_CONCEPTS[3].image}
+              title={CORE_CONCEPTS[3].title}
+              description={CORE_CONCEPTS[3].description}
+            ></CoreConcept>
+          </ul>
+        </section>
       </main>
     </div>
   );
