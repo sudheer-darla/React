63d12c7a847b9eee69b154041d99f82dfa7e95ec
sudheer-darla <sudheerkumarddsn@gmail.com>
2023-12-05 20:04:37 +0530

    Added left, right buttons to load pages - just controls- no functionality yet

--- ContextApiBasics/src/components/Home.jsx
+++ ContextApiBasics/src/components/Home.jsx
@@ -1,31 +1,34 @@
-import React from 'react';
-import { useEffect } from 'react';
-import { useState } from 'react';
+import React from "react";
+import { useEffect } from "react";
+import { useState } from "react";
 
-import ArrowCircleUpIcon from '@mui/icons-material/ArrowCircleUp';
-import ArrowCircleDownIcon from '@mui/icons-material/ArrowCircleDown';
-import basicOps from './utility/basicOps';
+import ArrowCircleUpIcon from "@mui/icons-material/ArrowCircleUp";
+import ArrowCircleDownIcon from "@mui/icons-material/ArrowCircleDown";
+import KeyboardArrowLeftIcon from "@mui/icons-material/KeyboardArrowLeft";
+import ChevronRightIcon from "@mui/icons-material/ChevronRight";
 
-import Categories from './Categories';
-import Productlist from './Productlist';
+import basicOps from "./utility/basicOps";
+
+import Categories from "./Categories";
+import Productlist from "./Productlist";
 
 function Home() {
-    /***single source of truth for all the products***/
-    const [products, setProducts] = useState([]);
+  /***single source of truth for all the products***/
+  const [products, setProducts] = useState([]);
 
-    /************ all the categories -> a product**********/
-    const [categories, setCategories] = useState([]);
+  /************ all the categories -> a product**********/
+  const [categories, setCategories] = useState([]);
 
-    /**********Action***********/
+  /**********Action***********/
 
-    /*********************** state ->term with which you want to filter the product list*****************************/
-    const [searchTerm, setSearchTerm] = useState("");
+  /*********************** state ->term with which you want to filter the product list*****************************/
+  const [searchTerm, setSearchTerm] = useState("");
 
-    /**************************sort : 0 : unsorted , 1: incresing order , -1 : decreasing order ************************************/
-    const [sortDirection, setSortDirection] = useState(0);
+  /**************************sort : 0 : unsorted , 1: incresing order , -1 : decreasing order ************************************/
+  const [sortDirection, setSortDirection] = useState(0);
 
-     /**************************** currcategory : category group you result **********************************/
-    const [currCategories, setCurrCategories] = useState(["All Categories"]);   
+  /**************************** currcategory : category group you result **********************************/
+  const [currCategories, setCurrCategories] = useState(["All Categories"]);
 
   useEffect(() => {
     (async function () {
@@ -41,42 +44,46 @@ function Home() {
   }, []);
 
   /**************getting all the categroies ********************/
-    useEffect(() => {
-        (async function () {
-            const resp = await fetch(`https://fakestoreapi.com/products/categories`)
-            const categoriesData = await resp.json();
-            setCategories(categoriesData);
-        })()
-    }, [])
+  useEffect(() => {
+    (async function () {
+      const resp = await fetch(`https://fakestoreapi.com/products/categories`);
+      const categoriesData = await resp.json();
+      setCategories(categoriesData);
+    })();
+  }, []);
 
-    let modifiedArray = basicOps(products, searchTerm, sortDirection, currCategories);   
-  
+  let modifiedArray = basicOps(
+    products,
+    searchTerm,
+    sortDirection,
+    currCategories
+  );
 
   return (
     <>
-      <header className='nav_wrapper'>
-        <div className='searchSort_wrapeer'>
+      <header className="nav_wrapper">
+        <div className="searchSort_wrapeer">
           <input
-            className='search_input'
-            type='text'
+            className="search_input"
+            type="text"
             value={searchTerm}
             onChange={(e) => {
               setSearchTerm(e.target.value);
             }}
           />
 
-          <div className='icon-container'>
-            {' '}
+          <div className="icon-container">
+            {" "}
             <ArrowCircleUpIcon
-              style={{ color: 'white' }}
-              fontSize='large'
+              style={{ color: "white" }}
+              fontSize="large"
               onClick={() => {
                 setSortDirection(1);
               }}
             ></ArrowCircleUpIcon>
             <ArrowCircleDownIcon
-              style={{ color: 'white' }}
-              fontSize='large'
+              style={{ color: "white" }}
+              fontSize="large"
               onClick={() => {
                 setSortDirection(-1);
               }}
@@ -85,17 +92,22 @@ function Home() {
         </div>
 
         <div className="categories_wrapper">
-                    <Categories
-                        categories={categories}
-                        setCurrCategories={setCurrCategories}
-                    />
-                </div>
+          <Categories
+            categories={categories}
+            setCurrCategories={setCurrCategories}
+          />
+        </div>
       </header>
 
       <main className="product_wrapper">
-                {/* products will be there */}
-                <Productlist productList={modifiedArray} />
-            </main>
+        {/* products will be there */}
+        <Productlist productList={modifiedArray} />
+      </main>
+
+      <div className="pagination">
+        <KeyboardArrowLeftIcon></KeyboardArrowLeftIcon>
+        <ChevronRightIcon></ChevronRightIcon>
+      </div>
     </>
   );
 }
