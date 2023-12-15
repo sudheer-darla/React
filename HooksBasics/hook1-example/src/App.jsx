9af78524c74831ea3385ba0fceb4f8ffe255e555
Sudheer Darla <sudheerkumarddsn@gmail.com>
2023-12-15 22:13:44 +0530

    Added another component little complex one

--- HooksBasics/hook1-example/src/App.jsx
+++ HooksBasics/hook1-example/src/App.jsx
@@ -1,13 +1,15 @@
 import './App.css';
 import CounterUseReducer from './components/UseReducer';
 import Counter from './components/UseStateExample';
+import Form from './components/UseReducerComplex';
 
 function App() {
   return (
     <>
       <h2>Use state example</h2>
       {/* <Counter /> */}
-      <CounterUseReducer />
+      {/* <CounterUseReducer /> */}
+      <Form />
     </>
   );
 }
